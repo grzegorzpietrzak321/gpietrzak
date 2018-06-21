1b49891322617a1246bb9d9502ba5ee6e1501e11
Grzegorz Pietrzak <grzegorz.pietrzak321@gmail.com>
2018-06-21 11:29:58 +0200

    CV page added, bootstrap theme added

--- blog/views.py
+++ blog/views.py
@@ -4,7 +4,16 @@ from django.contrib.auth.models import User
 
 
 def home(request):
+    posted_articles = list(Article.objects.all())
+    username = User.first_name
+
+    return render(request, "blog/home.html",
+                  {'articles': posted_articles}, {'user': username})
+
+
+def getall(request):
     all_articles = list(Article.objects.all())
     username = User.first_name
 
-    return render(request, "blog/home.html", {'articles': all_articles}, {'user': username})
+    return render(request, "blog/home.html",
+                  {'articles': all_articles}, {'user': username})
